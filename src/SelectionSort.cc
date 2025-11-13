d4fccdf651505e8e28ee95f06eb984f15f1e4b40
gbloesch <gbloesch@villanova.edu>
2025-11-13 20:35:22 +0000

    selection sort by gwen bloesch :)

--- src/SelectionSort.cc
+++ src/SelectionSort.cc
@@ -1,5 +1,14 @@
 #include "myheaders.h"
-
-
+//GWEN BLOESCH
 // Your BSort function(s) go here
-
+template<class T>
+void selectionsort(T data[], int n) {
+	for (int i = 0,j,least; i < n-1; i++) {
+		for (j= i+1, least =i; j < n; j++) 
+			if (data[j] < data[least] )
+				least = j;
+		if (i != least) {
+			swap(data[least], data[i]);
+		}
+	}
+}

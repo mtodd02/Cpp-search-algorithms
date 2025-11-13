6cd1f3a1d41616b6179207554a9a63fb8ff7255b
Miles-Haynes <mhayne03@villanova.edu>
2025-11-13 20:41:25 +0000

    changes to Insertion sort file

--- src/InsertionSort.cc
+++ src/InsertionSort.cc
@@ -1,4 +1,16 @@
 #include "myheaders.h"
+#include ""
+
+// Your Insertion Sort function(s) goes here ..
+
+template<class T>
+	void insertionsort(T data[], int n) {
+		for (int i = 1,j; i < n; i++) {
+			T tmp = data[i];
+		for (j = i; j > 0 && tmp < data[j-1]; j--)
+			data[j] = data[j-1];
+			data[j] = tmp;
+		}
+	}
 
 
-// Your ASort function(s) goes here ..

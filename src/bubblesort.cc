d7ff5d7911a263d71a536f30c82ccc4fe4c6b063
mtodd02 <mtodd02@villanova.edu>
2025-11-20 14:49:57 -0500

    bubble

--- src/bubblesort.cc
+++ src/bubblesort.cc
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-//void bubblesort(T data[], int n) {
-	//for (int i = 0; i < n-1; i++)
-		//for (int j = n-1; j > i; --j)
-			//if (data[j] < data[j-1])
-				//swap(data[j],data[j-1]);
-=======
 #include "myheaders.h"
 using namespace std;
 #include <fstream>
-//michael todd
+//Michael todd
 //bubble
 
 void bubblesort(long data[], int n) {
@@ -16,5 +9,5 @@ void bubblesort(long data[], int n) {
 		for (int j = n-1; j > i; --j)
 			if (data[j] < data[j-1])
 				swap(data[j],data[j-1]);
->>>>>>> branch 'main' of git@github.com:mtodd02/Cpp-search-algorithms.git
+
 }
